6a61431947e4d230c8eb8bb1e1c7b211b0915d63
Peter-kunosu <petersondergaard1@gmail.com>
2025-11-07 13:34:42 +0100

    Lavede klassen User

--- src/User.java
+++ src/User.java
@@ -1,2 +1,53 @@
-public class User {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class User{
+String username;
+String password;
+ArrayList<Media> watchedMovies;
+ArrayList<ArrayList<Media>> watchedSeries;
+ArrayList<Media> savedMovies;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.watchedMovies = new ArrayList<>();
+        this.watchedSeries = new ArrayList<>();
+        this.savedMovies = new ArrayList<>();
+    }
+
+    public void showWatchedMovies(){
+        if(watchedMovies.isEmpty()){
+            System.out.println("No watched movies");
+        } else {
+            System.out.println("Watched movies:");
+            for (Media m : watchedMovies) {
+                System.out.println(" - " + m);
+            }
+        }
+    }
+
+    public void showWatchedSeries(){
+        if(watchedSeries.isEmpty()){
+            System.out.println("No watched series");
+        } else {
+            System.out.println("Watched series:");
+            for (ArrayList<Media> S : watchedSeries) {
+                System.out.println(" - " + S);
+            }
+        }
+    }
+
+    public void showSavedMovies(){
+        if(savedMovies.isEmpty()){
+            System.out.println("No saved movies");
+        }
+        else {
+            System.out.println("Saved movies:");
+            for (Media sM : savedMovies) {
+                System.out.println(" - " + sM);
+            }
+        }
+    }
 }
+
