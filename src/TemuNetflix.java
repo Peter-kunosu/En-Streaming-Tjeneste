597bf823ee8d3855e5f7b133ac1d130a9de5af7f
jonasE-10 <jonas.ellekjaer10@gmail.com>
2025-11-07 14:18:20 +0100

    started on StartMenu added on TemuNetflix

--- src/TemuNetflix.java
+++ src/TemuNetflix.java
@@ -1,2 +1,18 @@
+import util.TextUI;
+import util.FileIO;
+
+import java.util.ArrayList;
+
 public class TemuNetflix {
+    private ArrayList<Media> media;
+    private ArrayList<String> options;
+
+    TextUI ui = new TextUI();
+    FileIO io = new FileIO();
+
+    private StartMenu startMenu;
+
+    public void startStartMenu(){
+
+    }
 }
