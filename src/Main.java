45dd3e1f3ed18d6c717e3e3d2d5b4fdd5caa8bb7
Peter-kunosu <petersondergaard1@gmail.com>
2025-11-10 15:31:03 +0100

    mergede branch med main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import entity.*;
+import util.*;
+
 public class Main {
     public static void main(String[] args) {
         TemuNetflix netflix = new TemuNetflix();
-        netflix.loadMediaData("src/Data/serier.txt");
-        System.out.println(netflix.media.get(0));
-        //netflix.ui.displayMsg(String.format(netflix.media.get(0).toString()));
+
+        StartMenu startMenu = new StartMenu();
+
+        StartMenu.start();
     }
 }
