6492483c8271ad89def8eda15846af14635a830d
Peter-kunosu <petersondergaard1@gmail.com>
2025-11-11 13:33:45 +0100

    laver videre i branch

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,15 @@ import util.*;
 
 public class Main {
     public static void main(String[] args) {
+        TextUI ui = new TextUI();
         TemuNetflix netflix = new TemuNetflix();
 
         StartMenu startMenu = new StartMenu();
 
         StartMenu.start();
+
+        //options til at kunne vælge imellem film, serier, sete film, sete serier.
+
+
     }
 }
